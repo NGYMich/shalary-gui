9010fcfd01cde0a62b7f22614106e76b01d94eab
NGYMich <ngmich95@gmail.com>
2023-04-05 19:45:54 +0200

    scrolldown to last element, animation, etc

--- src/app/routes/authentication/login/login.component.ts
+++ src/app/routes/authentication/login/login.component.ts
@@ -78,7 +78,7 @@ export class LoginComponent implements OnInit {
       },
       err => {
         console.log('error during onSubtmi() ', err.error.message)
-        this.errorMessage = err.error.message.startsWith("Bad credentials") ? "Wrong username or password" : err.error.message;
+        this.errorMessage = err.error.status == 401 ? "Wrong username or password" : err.error.error;
         this.isLoginFailed = true;
       }
     );
