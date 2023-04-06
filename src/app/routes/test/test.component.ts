c331ff49d501ce11a3f892f8fc632f6b5ba71bc8
NGYMich <ngmich95@gmail.com>
2023-04-06 11:11:47 +0200

    various mobile

--- src/app/routes/test/test.component.ts
+++ src/app/routes/test/test.component.ts
@@ -48,32 +48,10 @@ import {LocationService} from "../../services/LocationService";
   ]
 })
 export class TestComponent implements OnInit {
+  vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
 
-  introductionMessage =
-    `
-        People do not know what their salary expectations should be. Some underestimate their market value, and some overestimate it : it can be very difficult to get accurate
-        data. Well good news, Shalary is here to save you !
-    `
 
-  introductionMessage2 =
-    `
-        You need access to salaries. It's a taboo around you. No one talks about their current or past salaries. Well, here, everyone can view eachother's careers and everything is anonymous. What else to ask ?
-    `
-
-  introductionMessage3 =
-    `
-        You will be able to obtain interesting data thanks to the various testimonies of the users of the site.
-        For
-        example, you want to know the average salary of a data scientist after a master and 5 years of experience
-        in
-        Paris in consulting firms?
-        No problem, Shalary is here for you.
-    `
-
-  introductionMessage4 =
-    `
-        There also will be articles about different topics : negociations, formations, learning, data analytics, economics, and much more. Beware !
-    `
   isOpen = true;
   countriesControl = new FormControl("", (Validators.required))
   @Input() filteredCountries: Observable<Country[]>;
@@ -92,6 +70,7 @@ export class TestComponent implements OnInit {
   ngOnInit(): void {
     this.loadCountriesWithFlag()
     this.countriesControl = new FormControl('')
+    document.documentElement.style.setProperty('--vh', `${this.vh}px`);
   }
 
   private loadCountriesWithFlag() {
