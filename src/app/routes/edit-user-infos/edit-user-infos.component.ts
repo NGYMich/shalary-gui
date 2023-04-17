033baa135bf65e25f23fb9a1141aadaaa480b8bd
NGYMich <ngmich95@gmail.com>
2023-04-17 17:48:35 +0200

    data

--- src/app/routes/edit-user-infos/edit-user-infos.component.ts
+++ src/app/routes/edit-user-infos/edit-user-infos.component.ts
@@ -47,7 +47,7 @@ export class EditUserInfosComponent implements OnInit {
       }
     )
 
-    this.workHistoryFormComponent.salaryInfosForm.markAsTouched()
+    this.workHistoryFormComponent?.salaryInfosForm?.markAsTouched()
   }
 
   modifyUser($event: MouseEvent) {
